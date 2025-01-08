99e318143c3feaf416b8ac29296594e380c9994e
Arnold-pen <pengqi8535317@gmail.com>
2025-01-08 15:43:34 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,28 @@ import MacBookAirPage from './pages/MacBookAirPage';
 import MacMiniPage from './pages/MacMiniPage';
 import IMacPage from './pages/IMacPage';
 import AboutPage from './pages/AboutPage';
+import AdSenseVerification from './components/AdSenseVerification';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors flex flex-col">
-      <Navigation />
-      
-      <div className="flex-grow pt-16">
-        <Routes>
-          <Route path="/" element={<ProcessorPage />} />
-          <Route path="/macbook-pro" element={<MacBookProPage />} />
-          <Route path="/macbook-air" element={<MacBookAirPage />} />
-          <Route path="/mac-mini" element={<MacMiniPage />} />
-          <Route path="/imac" element={<IMacPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </div>
+    <>
+      <AdSenseVerification />
+      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors flex flex-col">
+        <Navigation />
+        
+        <div className="flex-grow pt-16">
+          <Routes>
+            <Route path="/" element={<ProcessorPage />} />
+            <Route path="/macbook-pro" element={<MacBookProPage />} />
+            <Route path="/macbook-air" element={<MacBookAirPage />} />
+            <Route path="/mac-mini" element={<MacMiniPage />} />
+            <Route path="/imac" element={<IMacPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 }
